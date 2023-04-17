810be3520756537acc9fdc5d37a853316023bb8d
embresse <115603992+embresse@users.noreply.github.com>
2023-04-17 11:28:49 -0400

    setting up files with json and initial code for components and app.js

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const mailIcon = <FontAwesomeIcon icon={faEnvelope} size="2xl" />
+const gitHub = <FontAwesomeIcon icon={faGithub} size="2xl" />
+
+export default function Footer() {
+    return (
+    <div className="fixed-bottom footer">
+      <div>
+        <ul className='nav justify-content-center'>
+          <li className='nav-item p-3'>
+            <a href="mailto: embresse@gmail.com" target="_blank">
+              {mailIcon}
+            </a>
+          </li>
+          <li className='nav-item p-3'>
+            <a href="https://github.com/embresse" target="_blank">
+              {gitHub}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    );
+};
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    const renderPage = () => {
+        if (currentPage === 'AboutMe') {
+            return <AboutMe />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        return <Resume />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+            <div className='container p-3'>
+            {renderPage()}
+            </div>
+        </div>
+    );
+}
