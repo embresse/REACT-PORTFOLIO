ac058a1d6fdb7a05c6abfef72f14119cbe22f58f
embresse <115603992+embresse@users.noreply.github.com>
2023-04-17 14:45:57 -0400

    aboutme.js edits

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,16 +5,19 @@ export default function AboutMe() {
   return (
     <div className="p-3 row">
       <div className="col-6">
-        <div className="card">
+        <div className="card shadow">
           <img src={image}></img>
         </div>
       </div>
       <div className="col-6">
         <div>
+          <h3>Background</h3>
           <p>
-            A graduate of Virginia Tech who spent 5 years teaching ESL, exploring, taking on a variety of different professional endeavors, and currently taking on web development!
+            A graduate of Virginia Tech who spent 5 years teaching ESL,
+            exploring, taking on a variety of different professional endeavors,
+            and currently taking on web development!
           </p>
-          <h3>SKILLS</h3>
+          <h3>What I've learned so far..</h3>
           <p>
             HTML, CSS, JavaScript, MongoDB, JQuery, SQL, Node, Express, NoSQL,
             Handlebars, React, and more!
